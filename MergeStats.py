a56ea4e71787af806cf088d6a98fbebf15ceb1ca
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2018-08-19 21:43:56 -0400

    Proper Drawing of Stats
    Stats are now drawn from relatively unaltered extracts from FilePackManager (the only change is that the are converted to csv format instead) and saved into a convenient dictionary. Next step will be to have all programs draw from this single source and remove the other files.

--- MergeStats.py
+++ MergeStats.py
@@ -1,17 +1,60 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import re
+import linecache
 
-stat_file = open('UnitStats.csv', 'r')
 
-armor = []
+for i,l in enumerate(linecache.getline("Land_Units_Tables.csv",3).split("\t")):
+    print(i,l)
+    
+for i,l in enumerate(linecache.getline("Melee_Weapons_Tables.csv",3).split("\t")):
+    print(i,l)
+    
+stat_file = open('Land_Units_Tables.csv', 'r')
+weapon_file = open('Melee_Weapons_Tables.csv', 'r')
 
-M = stat_file.readline()
-print(M.split("\t"))
+
+units = dict()
+weapons = dict()
+units['charge'] = []
+units['armor'] = []
+units['category'] = []
+units['class'] = []
+units['melee_A'] = []
+units['melee_D'] = []
+units['BVL'] = []
+units['BVI'] = []
+units['damage'] = []
+units['ap_damage'] = []
+units['attack_interval'] = []
+units['name'] = []
+
+
+
+for i,line in enumerate(weapon_file):
+    if i > 2:
+        L = line.split("\t")
+        weapons[L[3]] = [L[1],L[2],L[4],L[5],L[19]]
 
 for i,line in enumerate(stat_file):
-    if i > 0:
+    if i > 2:
         L = line.split("\t")
-        armor.append(int(re.findall("\d+$",L[0])[0]))
-    
-    
+        units['armor'].append(int(re.findall("\d+$",L[1])[0]))
+        units['charge'].append(int(L[4]))
+        units['category'].append(L[3])
+        units['class'].append(L[5])
+        units['melee_A'].append(int(L[12]))
+        units['melee_D'].append(int(L[13]))
+        units['name'].append(L[9])
+
+        weap = weapons[L[20]]
+        units['BVL'].append(int(weap[0]))
+        units['BVI'].append(int(weap[1]))
+        units['damage'].append(int(weap[2]))
+        units['ap_damage'].append(int(weap[3]))
+        units['attack_interval'].append(float(re.sub("\n","",weap[4])))
+
+
+for stat,val in units.items():
+    print(stat,val[71])
+
+np.save('unitsDictionary.npy', units)
---
+++ ReadDictionary.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+units = np.load('unitsDictionary.npy').item()
+
+for stat,val in units.items():
+    print(stat,val[71])
